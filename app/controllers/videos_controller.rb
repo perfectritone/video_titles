cf3c2d3b08203497069661949ea8c911afe1066d
David Frey <dfrey@zipcar.com>
2015-12-21 01:19:40 -0500

    master - Add HTML view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,8 +2,11 @@ class VideosController < ApplicationController
 
   def title
     video = Video.new(id: params[:video_id], source: Vimeo)
-    title = video.get_title
+    @title = video.get_title
 
-    render json: { title: title }
+    respond_to do |format|
+      format.json { render json: { title: @title } }
+      format.html
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails'
 require 'action_controller/railtie'
 require 'active_record/railtie'
+require 'action_view/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
